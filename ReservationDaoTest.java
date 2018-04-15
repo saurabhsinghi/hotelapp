02bec6946499d15f43c66402810090f49c2a56fe
saurabhsinghi <6396599+saurabhsinghi@users.noreply.github.com>
2018-04-15 15:52:11 -0700

    Add files via upload

---
+++ ReservationDaoTest.java
@@ -0,0 +1,114 @@
+package com.hotels.dao;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hotels.model.ReservationBean;
+import junit.framework.Assert;
+import org.apache.tika.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/hotel-servlet.xml")
+public class ReservationDaoTest {
+  @SuppressWarnings("SpringJavaAutowiringInspection")
+  @Autowired
+  ReservationDao reservationDao;
+
+  public ReservationBean convertJSONtoBean(String jsonFile) throws Exception{
+    ObjectMapper mapper = new ObjectMapper();
+    ReservationBean obj;
+    obj = mapper.readValue(IOUtils.toString(this.getClass().getResourceAsStream(jsonFile)), ReservationBean.class);
+    return  obj;
+  }
+
+  @Test
+  public void testReserveRoomWithInvalidHotelId() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/valid.json");
+    int result = reservationDao.reserveRoomInHotel(0, bean);
+    Assert.assertTrue(result == 0);
+  }
+
+  @Test (expected = DataIntegrityViolationException.class)
+  public void testReserveRoomWithMissingCheckin() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/missing_checkin.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test (expected = DataIntegrityViolationException.class)
+  public void testReserveRoomWithMissingCheckout() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/missing_checkout.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test (expected = DataIntegrityViolationException.class)
+  public void testReserveRoomWithMissingEmail() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/missing_email.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test (expected = DataIntegrityViolationException.class)
+  public void testReserveRoomWithMissingFName() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/missing_firstname.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test (expected = DataIntegrityViolationException.class)
+  public void testReserveRoomWithMissingLName() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/missing_lastname.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test (expected = Exception.class)
+  public void testReserveRoomWithInvalidDateFormat() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/invalid_date_format.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==0);
+  }
+
+  @Test
+  public void testReserveRoomWithInvalidDateRange() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/invalid_daterange.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==1);
+  }
+
+  @Test
+  public void testReserveRoomWithInvalidDateRangeSameInAndOut() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/invalid_daterange_same_inout.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==1);
+  }
+
+  @Test
+  public void testReserveRoomWithInvalidDateRangePast() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/invalid_daterange_past.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==1);
+  }
+
+
+  @Test
+  public void testReserveRoomWithInvalidEmail() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/invalid_email.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==1);
+  }
+
+  @Test
+  public void testReserveRoomValid() throws Exception{
+    ReservationBean bean = convertJSONtoBean("/valid.json");
+    int result = reservationDao.reserveRoomInHotel(100, bean);
+    Assert.assertTrue(result==1);
+  }
+}
